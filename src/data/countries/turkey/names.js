1d933fe2d687235fb88f4b2fa5e43e49b126aca8
Abdullah Talha Tülü <45394234+atalhatulu@users.noreply.github.com>
2026-09-21 21:02:27 +0300

    feat: add Türkiye 2026 country data profile

---
+++ src/data/countries/turkey/names.js
@@ -0,0 +1,9 @@
+export const FIRST_NAMES={
+ female:['Zeynep','Elif','Defne','Derya','Selin','Ece','Merve','Aylin','İrem','Melis','Seda','Ceren','Azra','İlayda','Buse','Gizem','Nisa','Yağmur','Esra','Sıla'],
+ male:['Emir','Kerem','Mert','Can','Deniz','Arda','Baran','Eren','Berk','Oğuz','Kaan','Umut','Yiğit','Burak','Onur','Emre','Alp','Batuhan','Furkan','Tolga']
+};
+
+export const SURNAMES=[
+ 'Yılmaz','Kaya','Demir','Şahin','Aydın','Arslan','Koç','Çelik','Öztürk','Yıldız',
+ 'Aksoy','Kurt','Özdemir','Aslan','Doğan','Kılıç','Çetin','Kara','Polat','Güneş'
+];
