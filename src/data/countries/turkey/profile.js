54ade1e2fb8cb003dcec9567ca70b7cde259cf80
Abdullah Talha Tülü <45394234+atalhatulu@users.noreply.github.com>
2026-09-21 21:02:45 +0300

    feat: add Türkiye 2026 country data profile

---
+++ src/data/countries/turkey/profile.js
@@ -0,0 +1,22 @@
+import {FIRST_NAMES,SURNAMES} from './names.js';
+import {TURKEY_CITIES,pickBirthCity,cityById,locationProfile} from './cities.js';
+import {SCHOOL_PREFIXES,UNIVERSITY_NAMES,UNIVERSITY_PROGRAMS} from './education.js';
+import {JOBS} from './jobs.js';
+import {TURKEY_2026_ECONOMY} from './economy.js';
+
+export const TURKEY_PROFILE={
+ id:'TR',
+ name:'Türkiye',
+ startYear:2026,
+ currency:'TRY',
+ firstNames:FIRST_NAMES,
+ surnames:SURNAMES,
+ cities:TURKEY_CITIES,
+ schoolPrefixes:SCHOOL_PREFIXES,
+ universityNames:UNIVERSITY_NAMES,
+ universityPrograms:UNIVERSITY_PROGRAMS,
+ jobs:JOBS,
+ economy:TURKEY_2026_ECONOMY
+};
+
+export {pickBirthCity,cityById,locationProfile};
